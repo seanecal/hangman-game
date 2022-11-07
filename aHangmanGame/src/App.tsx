b53781f00b852eedfb39e58c4a96e2a435bad6e0
Sen Cao <caosen.cn@gmail.com>
2022-11-07 23:28:35 +0200

    initialized three conponents

--- aHangmanGame/src/App.tsx
+++ aHangmanGame/src/App.tsx
@@ -1,11 +1,31 @@
 import { useState } from "react"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 import words from "./wordlist.json"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
-  return <h1>Hi</h1>
+  const [guessedLetters, setGuessedLetters] = useState(<string[]>([]))
+  return <div
+    style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}
+    >
+      <div style={{fontSize: "2rem", textAlign: "center"}}>
+       Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+  </div>
 }
 
 export default App
