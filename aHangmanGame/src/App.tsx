852ae97343d6b88c9d9175f79d059f37cf479022
Sen Cao <caosen.cn@gmail.com>
2022-11-07 23:18:30 +0200

    simple style to the layout added

--- aHangmanGame/src/App.tsx
+++ aHangmanGame/src/App.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react"
+import words from "./wordlist.json"
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
   return <h1>Hi</h1>
 }
 
