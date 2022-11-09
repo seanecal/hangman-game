fe43ce04a7ca390ea21bfa4793deeec733b6e8e3
Sen Cao <caosen.cn@gmail.com>
2022-11-09 12:07:58 +0200

    finished the hangmanword component

--- aHangmanGame/src/HangmanWord.tsx
+++ aHangmanGame/src/HangmanWord.tsx
@@ -1,3 +1,20 @@
 export function HangmanWord() {
-
+  const word = "text"
+  const guessedLetters = "e"
+  return(
+    <div style={{
+      display: "flex",
+      textTransform: "uppercase",
+      gap: ".25em",
+      fontWeight: "bolder",
+      fontSize: "5rem",
+      fontFamily: "sans-serif"
+    }}>
+    {word.split("").map((letter, index) => (
+      <span style={{ borderBottom: "10px solid black" }} key={index}>
+        <span style={{ visibility: guessedLetters.includes(letter)? "visible" : "hidden" }}>{letter}</span>
+      </span>
+    ))}
+    </div>
+  )
 }
