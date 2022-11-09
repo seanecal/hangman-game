01f2529080d7cf1049a61bc60c913b14b238cb02
Sen Cao <caosen.cn@gmail.com>
2022-11-09 12:56:10 +0200

    keyboard component finished

--- aHangmanGame/src/Keyboard.tsx
+++ aHangmanGame/src/Keyboard.tsx
@@ -1,3 +1,17 @@
-export function Keyboard() {
+import styles from "./Keyboard.module.css";
 
+const KEYS =  "abcdefghijklmnopqrstuvwxyz".split("")
+
+export function Keyboard() {
+  return (
+    <div>
+      {KEYS.map(key => {
+        return(
+          <button className={styles.btn} key={key}>
+            {key.toUpperCase()}
+          </button>
+        )
+      })}
+    </div>
+  )
 }
